3df6edca35c4d803869b6be06454916c5a5aa47b
Hariprathap-converse <hariprathap@conversedatasolutions.com>
2025-10-08 18:21:00 +0530

    add example for Multiple Page

--- src/app/components/rout-buttons.tsx
+++ src/app/components/rout-buttons.tsx
@@ -12,6 +12,7 @@ export default function PageButtons() {
     { path: "/both", label: "Both" },
     { path: "/diffrent", label: "Diffrent" },
     { path: "/size-different", label: "Size Different" },
+    { path: "/multiple", label: "Multiple Page" },
   ];
 
   return (
